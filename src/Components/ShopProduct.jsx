abeee47db4dc56571536e5f86df4885bd18138c1
MdShahinDev <hello.shahinofficial@gmail.com>
2025-01-16 17:01:23 +0600

    Per Page

--- src/Components/ShopProduct.jsx
+++ src/Components/ShopProduct.jsx
@@ -57,30 +57,38 @@ const ShopProduct = () => {
   const handleGoToPage = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
-  const handlePerPage =(e)=>{
-    setPerPage(e.target.value);
-  }
+  const handlePerPage = (e) => {
+    setPerPage(e.target.value || 30);
+  };
   return (
     <>
-    <div className="shopHeader container mx-auto py-16 flex flex-col sm:flex-col md:flex-row justify-between items-start md:items-center gap-4">
+      <div className="shopHeader container mx-auto py-16 flex flex-col sm:flex-col md:flex-row justify-between items-start md:items-center gap-4">
         <div className="title font-Crimson font-semibold text-2xl">
-        Ecommerce Acceories & Fashion item <br /><span className='text-sm text-gray-400'>About 9,620 results (0.62 seconds)</span>
+          Ecommerce Acceories & Fashion item <br />
+          <span className="text-sm text-gray-400">
+            About 9,620 results (0.62 seconds)
+          </span>
         </div>
         <div className="number flex gap-2">
-            Per Page: 
-            <input onChange={handlePerPage} type="number" className="border pl-2" />
+          Per Page:
+          <input
+            onChange={handlePerPage}
+            type="number"
+            className="border pl-2"
+          />
         </div>
         <div className="sorted">
-            Sorted By: 
-            <select name="" className='ml-2 border p-2 focus:outline-none'>
-              <option value="">Best Match</option>
-              <option value="">Featured</option>
-            </select>
+          Sorted By:
+          <select name="" className="ml-2 border p-2 focus:outline-none">
+            <option value="">Best Match</option>
+            <option value="">Featured</option>
+          </select>
         </div>
         <div className="view flex gap-3 items-center">
-          View: <IoListSharp className='text-xl text-Primary cursor-pointer'/> <IoGrid className='text-lg text-Primary cursor-pointer'/>
+          View: <IoListSharp className="text-xl text-Primary cursor-pointer" />{" "}
+          <IoGrid className="text-lg text-Primary cursor-pointer" />
         </div>
-    </div>
+      </div>
 
       <div className="shop-page container mx-auto flex flex-col lg:flex-row gap-4">
         <div className="sidebar w-full px-2 lg:w-1/4 order-2 lg:order-1">
@@ -88,7 +96,12 @@ const ShopProduct = () => {
             <h2 className="font-Crimson text-2xl font-semibold mb-2">
               Search Product
             </h2>
-            <input onChange={handleSearch} type="text" className="py-2 px-1 w-full border rounded-sm" placeholder="Search Product" />
+            <input
+              onChange={handleSearch}
+              type="text"
+              className="py-2 px-1 w-full border rounded-sm"
+              placeholder="Search Product"
+            />
             {searchResult.length > 0 && (
               <div className="result absolute top-20  left-0 bg-red-400 px-2 mb-4 max-h-[500px] overflow-y-auto">
                 {searchResult.map((item) => (
@@ -111,8 +124,10 @@ const ShopProduct = () => {
             </h2>
             <ul className="flex flex-col gap-4 text-lg font-semibold ">
               {category.map((item) => (
-                <li onClick={() => handleCategoryProduct(item)}
-                  className="cursor-pointer capitalize hover:text-Primary">
+                <li
+                  onClick={() => handleCategoryProduct(item)}
+                  className="cursor-pointer capitalize hover:text-Primary"
+                >
                   {item}
                 </li>
               ))}
@@ -120,89 +135,111 @@ const ShopProduct = () => {
           </div>
         </div>
         <div className="product sm:w-1/2 p-4 lg:w-3/4 order-1 lg:order-2">
-        <div className="flex flex-wrap -mx-3">
-          {categoryItem.length > 0 ? (
-            categoryItem.map((item) => (
-              <div key={item.id} className="single-product sm:w-1/2 md:w-1/3 lg:w-1/4 px-2 mb-4">
-                <div className="bg-white shadow rounded relative group">
-                  <img className="mb-4 bg-[#f6f7fb] w-full cursor-pointer" src={item.thumbnail} alt="" />
-                <div className="quick-panel absolute top-48 left-4 flex flex-col gap-5 text-xl opacity-0 -translate-x-5      transition-all duration-300 group-hover:opacity-100 group-hover:translate-x-4">
-                  <IoSearch className="cursor-pointer hover:text-Primary transition-all" />
-                  <IoCartOutline className="cursor-pointer hover:text-Primary transition-all" />
-                  <FaRegHeart className="cursor-pointer hover:text-Primary transition-all" />
-              </div>
-           
-            <div className="product-title text-center mb-4">
-              <h2 className="font-Crimson text-2xl font-semibold transition-all hover:text-Primary cursor-pointer">
-                {item.title}
-              </h2>
-            </div>
-            <div className="product-sku text-center mb-4">
-              <p>
-                Product SKU: <span className="text-Primary">{item.sku}</span>
-              </p>
-            </div>
-            <div className="product-price text-center mb-4 pb-2 text-xl font-semibold">
-              <p>${item.price}</p>
-            </div>
-          </div>
-        </div>
-      ))) : (
-        <div className="flex flex-wrap -mx-3">
-          {currentPageProduct.map((item) => (
-            <div key={item.id} className="single-product sm:w-1/2 md:w-1/3 lg:w-1/4 px-2 mb-4">
-              <div className="bg-white shadow rounded relative group">
-                <img
-                  className="mb-4 bg-[#f6f7fb] w-full cursor-pointer"
-                  src={item.thumbnail}
-                  alt=""
-                />
-                <div className="quick-panel absolute top-48 left-4 flex flex-col gap-5 text-xl opacity-0 -translate-x-5 transition-all duration-300 group-hover:opacity-100 group-hover:translate-x-4">
-                  <IoSearch className="cursor-pointer hover:text-Primary transition-all" />
-                  <IoCartOutline className="cursor-pointer hover:text-Primary transition-all" />
-                  <FaRegHeart className="cursor-pointer hover:text-Primary transition-all" />
-                </div>
-                <div className="product-title text-center mb-4">
-                  <h2 className="font-Crimson text-2xl font-semibold transition-all hover:text-Primary cursor-pointer">
-                    {item.title}
-                  </h2>
-                </div>
-                <div className="product-sku text-center mb-4">
-                  <p>
-                    Product SKU: <span className="text-Primary">{item.sku}</span>
-                  </p>
-                </div>
-                <div className="product-price text-center mb-4 pb-2 text-xl font-semibold">
-                  <p>${item.price}</p>
-                </div>
-              </div>
-            </div>
-          ))}
-        <div className="container mx-auto flex flex-wrap justify-center gap-8 py-20">
-          <div onClick={handlePrevPage} className="prev-button p-4 border cursor-pointer">
-            Previous
-          </div>
-          <div className="number">
-            <ul className="flex flex-wrap gap-4">
-              {pages.map((item) => (
-                <li
-                  key={item}
-                  onClick={() => handleGoToPage(item)}
-                  className={`p-4 border cursor-pointer ${
-                    item === currentPage ? "bg-blue-100" : ""
-                  }`}> {item} </li>
-              ))}
-            </ul>
-          </div>
-          <div onClick={handleNextPage} className="next p-4 border cursor-pointer">
-            Next
-          </div>
-        </div>
-      </div>
-    )}
-  </div>
-</div>
+          <div className="flex flex-wrap -mx-3">
+            {categoryItem.length > 0 ? (
+              categoryItem.map((item) => (
+                <div
+                  key={item.id}
+                  className="single-product sm:w-1/2 md:w-1/3 lg:w-1/4 px-2 mb-4"
+                >
+                  <div className="bg-white shadow rounded relative group">
+                    <img
+                      className="mb-4 bg-[#f6f7fb] w-full cursor-pointer"
+                      src={item.thumbnail}
+                      alt=""
+                    />
+                    <div className="quick-panel absolute top-48 left-4 flex flex-col gap-5 text-xl opacity-0 -translate-x-5      transition-all duration-300 group-hover:opacity-100 group-hover:translate-x-4">
+                      <IoSearch className="cursor-pointer hover:text-Primary transition-all" />
+                      <IoCartOutline className="cursor-pointer hover:text-Primary transition-all" />
+                      <FaRegHeart className="cursor-pointer hover:text-Primary transition-all" />
+                    </div>
 
+                    <div className="product-title text-center mb-4">
+                      <h2 className="font-Crimson text-2xl font-semibold transition-all hover:text-Primary cursor-pointer">
+                        {item.title}
+                      </h2>
+                    </div>
+                    <div className="product-sku text-center mb-4">
+                      <p>
+                        Product SKU:{" "}
+                        <span className="text-Primary">{item.sku}</span>
+                      </p>
+                    </div>
+                    <div className="product-price text-center mb-4 pb-2 text-xl font-semibold">
+                      <p>${item.price}</p>
+                    </div>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <div className="flex flex-wrap -mx-3">
+                {currentPageProduct.map((item) => (
+                  <div
+                    key={item.id}
+                    className="single-product sm:w-1/2 md:w-1/3 lg:w-1/4 px-2 mb-4"
+                  >
+                    <div className="bg-white shadow rounded relative group">
+                      <img
+                        className="mb-4 bg-[#f6f7fb] w-full cursor-pointer"
+                        src={item.thumbnail}
+                        alt=""
+                      />
+                      <div className="quick-panel absolute top-48 left-4 flex flex-col gap-5 text-xl opacity-0 -translate-x-5 transition-all duration-300 group-hover:opacity-100 group-hover:translate-x-4">
+                        <IoSearch className="cursor-pointer hover:text-Primary transition-all" />
+                        <IoCartOutline className="cursor-pointer hover:text-Primary transition-all" />
+                        <FaRegHeart className="cursor-pointer hover:text-Primary transition-all" />
+                      </div>
+                      <div className="product-title text-center mb-4">
+                        <h2 className="font-Crimson text-2xl font-semibold transition-all hover:text-Primary cursor-pointer">
+                          {item.title}
+                        </h2>
+                      </div>
+                      <div className="product-sku text-center mb-4">
+                        <p>
+                          Product SKU:{" "}
+                          <span className="text-Primary">{item.sku}</span>
+                        </p>
+                      </div>
+                      <div className="product-price text-center mb-4 pb-2 text-xl font-semibold">
+                        <p>${item.price}</p>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+                <div className="container mx-auto flex flex-wrap justify-center gap-8 py-20">
+                  <div
+                    onClick={handlePrevPage}
+                    className="prev-button p-4 border cursor-pointer"
+                  >
+                    Previous
+                  </div>
+                  <div className="number">
+                    <ul className="flex flex-wrap gap-4">
+                      {pages.map((item) => (
+                        <li
+                          key={item}
+                          onClick={() => handleGoToPage(item)}
+                          className={`p-4 border cursor-pointer ${
+                            item === currentPage ? "bg-blue-100" : ""
+                          }`}
+                        >
+                          {" "}
+                          {item}{" "}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                  <div
+                    onClick={handleNextPage}
+                    className="next p-4 border cursor-pointer"
+                  >
+                    Next
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
     </>
   );
